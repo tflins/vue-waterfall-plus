d18b9ec453c0af43fdd134c6c59c73ee43f9af03
tflin <tflins@163.com>
2020-02-16 21:58:56 +0800

    增加 waterfallItem 组件

---
+++ src/components/WaterfallPlus/WaterfallItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img
+      :src="src"
+      alt=""
+      :ref="`img${i}`"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  props: {
+    src: String
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped></style>
